c884015fd30aff96c39d2d8c9e70bcea42898abe
hajaramani09 <hajar6amani@gmail.com>
2024-01-23 16:31:29 +0100

    cm

--- src/main/java/codingtechniques/controller/ReservationController.java
+++ src/main/java/codingtechniques/controller/ReservationController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RestController
-@RequestMapping("/reservation")
+@RequestMapping("/")
 
 @Component
 
--- src/main/java/codingtechniques/controller/ResponsableController.java
+++ src/main/java/codingtechniques/controller/ResponsableController.java
@@ -20,7 +20,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @AllArgsConstructor
 
-@RequestMapping("/api/responsable")
+@RequestMapping("/")
 public class ResponsableController {
 
 	@Autowired
